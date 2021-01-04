b9abf855f6d5b59af6ef8c2c96498e264d7f8d5d
Samantha <samanthagottlieb31@gmail.com>
2021-01-04 12:46:30 +0000

    Add picture column to posts database table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_04_115420) do
+ActiveRecord::Schema.define(version: 2021_01_04_124410) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "picture"
   end
 
 end
